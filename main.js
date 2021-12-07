a9318a2755d788a8d7e9890757c44473bb8da7f3
ushica <85010308+ushica@users.noreply.github.com>
2021-12-07 20:57:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// Create a reference for the canvas
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+            {
+            aplhabetkey();
+            document.getElementById("d1").innerHTML
+            }
+		//write a code to check the type of key pressed
+    
+    
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet key";
+        console.log("alphabet key");
+	}
+}
+
+function aplhabetkey()
+{
+    img_image="Alpkey.png";
+    add();
+	//upload respective image with the message. 
+}
+function numberkey()
+{
+img_image="numkey.png";
+add();
+}
+function arrowkey()
+{
+    img_image="Arrkey.png";
+add();
+}
+function specialkey()
+{
+img_image="spkey.png";
+add();	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
